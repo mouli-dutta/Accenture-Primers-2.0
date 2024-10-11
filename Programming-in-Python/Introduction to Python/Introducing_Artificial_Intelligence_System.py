c0741187153281f64c2a4c257ac486983cdb07ec
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2024-10-11 13:31:29 +0530

    Create Introducing_Artificial_Intelligence_System.py

---
+++ Programming-in-Python/Introduction to Python/Introducing_Artificial_Intelligence_System.py
@@ -0,0 +1,11 @@
+name = input('Enter the name: ')
+creator = input('Enter the creator name: ')
+purpose = input('Enter the purpose: ')
+size = input('Enter the memory size: ')
+speed = input('Enter the speed: ')
+
+print('My Details:\n')
+
+print(f'I am {name}, created by {creator}, for the purpose of {purpose}.\n')
+
+print(f'Memory I consume is around {size} and my speed is {speed} GHz.')
