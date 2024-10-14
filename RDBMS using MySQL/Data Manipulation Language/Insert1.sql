a2711cc82f72fc0f41922a7798d36dc287dc4437
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2024-10-14 12:28:29 +0530

    Create Insert1.sql

---
+++ RDBMS using MySQL/Data Manipulation Language/Insert1.sql
@@ -0,0 +1,6 @@
+INSERT INTO Booking_Master(Booking_id, Enquiry_Id, Total_amount, Mode_of_Pay)
+VALUES
+('B101', 'En1001', 40000, 'Cash'),
+('B102', 'En1012', 750000, 'Online'),
+('B103', 'En1016', 16000, 'Cheque'),
+('B104', 'En1007', 10000, 'Cash');
